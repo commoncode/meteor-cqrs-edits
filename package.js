ee61161372df6a52bb8ff6cf3f1c9377b8dc8f85
Daryl Antony <daryl@commoncode.com.au>
2014-05-22 16:59:54 +1000

    WIP edits

--- package.js
+++ package.js
@@ -8,4 +8,10 @@ Package.on_use(function (api) {
   api.add_files('lib/client.js', 'client');
   api.add_files('lib/server.js', 'server');
 
+  api.export('EditDocuments', ['client', 'server']);
+  api.export('EditDiffs', ['client', 'server']);
+  api.export('editStates', 'server');
+  api.export('editActions', 'server');
+  api.export('editHelpers', ['client', 'server']);
+
 });
