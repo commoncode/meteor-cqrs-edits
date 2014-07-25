194cee7f67af4400b3c85a52c7128c78a2b27107
Y3K <dboy.y3k@gmail.com>
2014-07-24 22:26:34 -0500

    Cleanup, rearrange, widgets, etc, ect

--- package.js
+++ package.js
@@ -1,5 +1,3 @@
-console.log('cqrs-edits package.js');
-
 Package.describe({
   summary: "Edit Documents, Diffs and Operational Transforms for CQRS"
 });
@@ -9,26 +7,12 @@ Npm.depends({
 });
 
 Package.on_use(function (api) {
-
   api.use(['templating', 'moment'], 'client');
-
-  // Load this before client.js so we have Template loaded.
-  api.add_files('lib/client.html', 'client');
+  api.use('collection-helpers');
 
   api.add_files('lib/common.js', ['client', 'server']);
-  api.add_files('lib/client.js', 'client');
+  api.add_files(['lib/client.html', 'lib/client.js'], 'client');
   api.add_files('lib/server.js', 'server');
 
-
-  api.export('diffParts', 'client');
-  api.export('editActions', 'server');
-  api.export('EditDiffs', ['client', 'server']);
-  api.export('editDocFormControls', 'client');
-  api.export('editDocFormInput', 'client');
-  api.export('editDocsCursor', 'server');
-  api.export('EditDocuments', ['client', 'server']);
-  api.export('editEvents', 'client');
-  api.export('editHelpers', ['client', 'server']);
-  api.export('editStates', 'server');
-
+  api.export(['CQRS', 'EditDiffs', 'EditDocuments']);
 });
